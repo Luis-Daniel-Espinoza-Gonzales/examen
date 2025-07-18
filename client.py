3e2fe3714695c22dd48f928fb9ac951130ee61f2
Alejandra-Chavez <alejandrachavezu9@gmail.com>
2025-07-18 19:54:36 -0300

    subo

--- client.py
+++ client.py
@@ -1,5 +1,4 @@
 import socket
-import sys
 
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
@@ -10,26 +9,18 @@ print("La dirección IP de su computadora es: " + ip)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #AF.INET = IPv4, SOCK_STREAM = TCP.
 client_socket.connect((ip, 12345))  #se conecta a una ip, y un puerto definido que se usa para conectarse
 
-#Recibe pregunta del servidor
-respuesta = client_socket.recv(1024).decode()
-print(respuesta)
+while True:
+    #Recibe pregunta del servidor
+    respuesta = client_socket.recv(1024).decode()
 
-#Recibe pregunta del servidor
-pregunta_00 = client_socket.recv(1024).decode()
-respuesta_00 = input(pregunta_00 + " ")
+    #Recibe pregunta del servidor
+    respuesta_00 = input(respuesta + " ")
 
-#Envio respuesta
-client_socket.send(respuesta_00.encode())
-
-#Recibe pregunta del servidor
-pregunta_01 = client_socket.recv(1024).decode()
-respuesta_01 = input(pregunta_01 + " ")
-
-#Envio respuesta
-client_socket.send(respuesta_01.encode())
-
-mensaje_00 = client_socket.recv(1024).decode()
-print(mensaje_00)
+    #Envio respuesta
+    client_socket.send(respuesta_00.encode())
+    
+    if (respuesta_00 == "/adios"):
+        break
 
 #cierre de conexion
 client_socket.close()
--- server.py
+++ server.py
@@ -1,70 +1,77 @@
 import socket
 import requests
+import threading
+
+#Obtetncion de nombre e IP del equipo
+hostname = socket.gethostname() #Obtiene el nombre del equipo
+ip = socket.gethostbyname(hostname) #Traduce el nombre en una IP
 
 clave = {
     "usuario" : "daniel",
     "contraseña" : "den1"
 }
 
+def manejo(client_socket, address):
+    while True:
 
-#Obtetncion de nombre e IP del equipo
-hostname = socket.gethostname() #Obtiene el nombre del equipo
-ip = socket.gethostbyname(hostname) #Traduce el nombre en una IP
-print("El nombre de su computadora es: " + hostname)
-print("La dirección IP de su computadora es: " + ip)
+        try:
+            client_socket.send("hola, te envio saludo desde el servidor.".encode())
+
+            pregunta_00 = "Ingrese su usuario:"
+            client_socket.send(pregunta_00.encode())
+
+            #Recibir repuesta
+            respuesta_00 = client_socket.recv(1024).decode()
+            print("Usuario del cliente: ", respuesta_00)
+
+            #verifica el usuario y contraseña
+            if respuesta_00 == clave["usuario"]:
+                print("acceso concedido.")
+
+                #Envio algo
+                client_socket.send(b"\n=== Bienvenido al Chat ===\n1. /repos (para pedir los movimientos de combate de la region de kanto)\n2. /adios (fin de la comunicacion)\nElige una opcion: ")
+
+                #Recibir repuesta
+                respuesta_01 = client_socket.recv(1024).decode()
+
+                if respuesta_01 == "/repos":
+                    url = 'https://pokeapi.co/api/v2/generation/1/'
+                    data = requests.get(url)
+
+                    if(data.status_code == 200):
+                        data = data.json()
+                        asd_00 = data.get("moves")
+                        asd_01 = asd_00[0]["name"]
+
+                        print(asd_01)
+                        client_socket.send(f"Estos son todos los movimientos de la region de kanto {asd_01}".encode())
+                        
+
+                elif respuesta_01 == "/adios":
+                    mensaje_adios = "Hasta luego."
+                    client_socket.send(mensaje_adios.encode())
+                    client_socket.close()
+            else:
+                print("acceso denegado.")
+                client_socket.send("Acceso denegado. Cerrando conexión.".encode())
+                client_socket.close()
+        except Exception as e:
+            print(e)
+            break
+    client_socket.close()
+
 
 #Configuraciones del servidor
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #AF.INET = protocolo IPv4, SOCK_STREAM = tipo de socket TCP.
 server_socket.bind((ip, 12345)) #se asigna ip y puerto definido donde se escucha
-server_socket.listen(1) #Indica cuantas conexiones puede mantener
-
-print("server escuchando en el puerto 12345")
-
+server_socket.listen() #Indica cuantas conexiones puede mantener
 while True:
-
-    #Crea socket
+    
     client_socket, address = server_socket.accept()
-    print("se establecio una conexion con " + str(address))
+    print("server escuchando en el puerto 12345")
 
-    #Envio saludo
-    client_socket.send("hola, te envio saludo desde el servidor.".encode())
+    hilo = threading.Thread(target=manejo , args=(client_socket, address))
+    hilo.start()
 
-    #Envio pregunta
-    pregunta_00 = "Ingrese su usuario:"
-    client_socket.send(pregunta_00.encode())
-
-    #Recibir repuesta
-    respuesta_00 = client_socket.recv(1024).decode()
-    print("Usuario del cliente: ", respuesta_00)
-
-    #verifica el usuario y contraseña
-    if respuesta_00 == clave["usuario"]:
-        print("acceso concedido.")
-
-        #Envio algo
-        client_socket.send(b"\n=== Bienvenido al Chat ===\n1. /repos (para pedir los movimientos de combate de la region de kanto)\n2. /adios (fin de la comunicacion)\nElige una opcion: ")
-
-        #Recibir repuesta
-        respuesta_01 = client_socket.recv(1024).decode()
-        client_socket.send("Acceso concedido. Bienvenido.".encode())
-
-        if respuesta_01 == "/repos":
-            url = 'https://pokeapi.co/api/v2/generation/1/'
-            data = requests.get(url)
-
-            if(data.status_code == 200):
-                data = data.json()
-                client_socket.send("Estos son todos los movimientos de la region de kanto".encode())
-                cant = 0
-
-                print(data["moves"])
-                
-
-        elif respuesta_01 == "/adios":
-            mensaje_adios = "Hasta luego."
-            client_socket.send(mensaje_adios.encode())
-            client_socket.close()
-    else:
-        print("acceso denegado.")
-        client_socket.send("Acceso denegado. Cerrando conexión.".encode())
-        client_socket.close()
+            
+            
